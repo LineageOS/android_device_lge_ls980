3a8cb8e417b51749f52fffb2ea6b42242d13be14
Rashed Abdel-Tawab <rashed@linux.com>
2014-11-15 11:35:29 -0500

    ls980: NFC overhaul
    -Add NFC HAL
    -Add SE policy (also requires g2-common change)
    -No conf file needed
    
    Change-Id: I45593f33f6a4c6fc2d42c349d19a03de5bd8dd50

---
+++ nfc/nfc.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := device/lg/ls980
+
+PRODUCT_COPY_FILES += \
+    frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
+
+# NFC Support
+PRODUCT_PACKAGES += \
+    libnfc \
+    libnfc_jni \
+    Nfc \
+    Tag \
+    com.android.nfc_extras
